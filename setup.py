68f8aeaa1785a2ffc5e4ce1a908efc63d91d38b9
joheb2812 <johebdani@gmail.com>
2025-07-30 10:29:25 +0530

    All file updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Joheb Dani',
     author_email='johebdani@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["google-generativeai","langchain-community","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
